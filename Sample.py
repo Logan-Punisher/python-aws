ff7202f4d2f88e70bde2495f2b9a33c22e9297ff
EC2 Default User <ec2-user@ip-10-16-10-33.us-west-2.compute.internal>
2023-11-27 14:11:56 +0000

    2nd Commit

--- Sample.py
+++ Sample.py
@@ -1,1 +1,41 @@
-print("HELLO WOrld")
+import math
+
+num1 = int(input("Enter 1st Number: "))
+num2 = int(input("Enter 2nd Number: "))
+
+#Operation = int(input("\n1. Addition \n2. Subtraction \n3. Multiplication \n4. Division \n5. Modulo \n6. Raise to a power"))
+Operation = int(input('''1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+5. Modulo
+6. Raise to a power
+'''))
+
+if Operation == 1:
+    Addition = num1 + num2
+    print("the Addition of Number is: ",Addition)
+elif Operation == 2:
+    Subtraction = num1 - num2
+    print("the Subtraction of Number is: ",Subtraction)
+elif Operation == 3:
+    Multiplication = num1 * num2
+    print("the Multiplication of Number is: ",Multiplication)
+elif Operation == 4:
+    if num2 != 0:
+        Divison = num1 / num2
+        print("the Divison of Number is: ",Divison)
+    else:
+        print("Divison not Possible")
+elif Operation == 5:
+    if num2 != 0:
+        Modulo = num1 % num2
+        print("the Modulo of Number is: ",Modulo)
+    else:
+        print("Modulues not Possible")    
+elif Operation == 6:
+    Raise=math.pow(num1,num2)
+    print("the Raise to a power of Number is: ",Raise)
+else:
+    print("select Proper Option")
+    
